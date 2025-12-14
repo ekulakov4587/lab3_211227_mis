6455ce4793676f413ce2a40dcd313aea11aed14a
ekulakov4587 <149899466+ekulakov4587@users.noreply.github.com>
2025-12-14 17:58:42 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-// Firebase & messaging packages required:
-// firebase_core, firebase_messaging, flutter_local_notifications, firebase_auth
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
